8da3f76bb925369d218693961515889fd4c81a91
Sahil Kumar <sahil.kumar9274@gmail.com>
2023-12-15 19:02:46 +0530

    loops again

--- src/main/java/ArrayConcept.java
+++ src/main/java/ArrayConcept.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class ArrayConcept {
+public class ArrayConcept {
+
+    public static void main(String[] args) {
+
+
+        //static array
+       //can store similar type of data
+
+
+        int[] numbers = new int[4];
+        //li = 0
+        //hi = 3
+        //range = 0 to 3
+        //length = 4
+
+       // numbers[-1] = 30; //.ArrayIndexOutOfBoundsException AIOB
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+       // numbers[4] = 50; //.ArrayIndexOutOfBoundsException AIOB
+
+
+        System.out.println(numbers[0]);
+
+
+        double[] d = new double[4];
+
+        d[0] = 1.1;
+        d[1] = 2.2;
+        d[2] = 3.6;
+        d[3] = 4.8;
+
+
+
+        char[] c = new char[4];
+
+
+        c[0] = 'a';
+        c[1] = 'b';
+        c[2] = 'c';
+        c[3] = 'd';
+
+
+        String[] browsers = new String[4];
+
+        browsers[0] = "chrome";
+        browsers[1] = "firefox";
+        browsers[2] = "safari";
+        browsers[3] = "edge";
+
+
+
+        //employee
+
+        //age int, name String, active boolean, salary double
+
+
+        Object[] employee = new Object[4];
+
+        employee[0] = 32;
+        employee[1] = "Sahil";
+        employee[2] = true;
+        employee[3] = 20000.45;
+
+        System.out.println(employee[2]);
+
+
+
+
+
+    }
 }
--- src/main/java/ArrayTraversing.java
+++ src/main/java/ArrayTraversing.java
@@ -1,2 +1,80 @@
-package PACKAGE_NAME;public class ArrayTraversing {
+import java.util.Arrays;
+
+public class ArrayTraversing {
+
+    public static void main(String[] args) {
+
+
+        int[] numbers = new int[4];
+
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+
+
+        //System.out.println(Arrays.toString(numbers));
+        //System.out.println(numbers);
+
+
+        //To iterate and array : we need for loop
+
+
+       // index based loop
+
+        for (int i=numbers.length-1; i>=0; i--){
+
+            System.out.println(numbers[i]);
+
+        }
+
+
+        //for-each loop
+
+//        int[] numbers = new int[4];
+//
+//        numbers[0] = 10;
+//        numbers[1] = 20;
+//        numbers[2] = 30;
+//        numbers[3] = 40;
+//
+//        for (int e : numbers){
+//            if (e == 20)
+//            {
+//                System.out.println(e);
+//                break;
+//            }
+//            System.out.println(e);
+//        }
+
+//        double[] d = new double[4];
+//
+//        d[0] = 1.1;
+//        d[1] = 2.2;
+//        d[2] = 3.6;
+//        d[3] = 4.8;
+//
+//        for(double d1: d){
+//            System.out.println(d1);
+//        }
+
+
+//        String[] browsers = new String[4];
+//
+//        browsers[0] = "chrome";
+//        browsers[1] = "firefox";
+//        browsers[2] = "safari";
+//        browsers[3] = "edge";
+//
+//
+//        for(String s : browsers){
+//
+//            if(s.equals("chrome")){
+//                System.out.println("launch chrome");
+//            }
+//        }
+
+
+
+    }
 }
